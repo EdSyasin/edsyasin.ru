6beb18f4d662fd93556f45e635b9c4f555bd7518
Эдуард Сясин <edward.syasin@gmail.com>
2018-10-21 14:40:42 +0300

    Revert "Revert "mainpage""
    This reverts commit 1266e8ad09a6ac609d2a4acd3158ce8c6f792dc4.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueMouseParallax from 'vue-mouse-parallax'
+import style from './style.css'
+import normalize from './normalize.css'
+
+
+
+Vue.use(VueRouter)
+Vue.use(VueMouseParallax)
+
+//Views
+import Main from './views/main/main.vue'
+
+var router = new VueRouter({
+  routes:[
+    { path: '/', name: 'main', component: Main},
+  ]
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router: router,
+  
 })
