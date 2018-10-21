1b649abc687f108ee15b8b8ad066f9e327e116f2
Эдуард Сясин <edward.syasin@gmail.com>
2018-10-21 16:27:34 +0300

    BG-open

---
+++ src/components/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="men">
+    
+    </div>    
+</template>
+
+<script>
+export default {
+    name:'mainmenu'
+}
+</script>
+
+<style scoped>
+#men{
+    position: absolute;
+    background: brown;
+    height: 100%;
+    width: 100%;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import VueMouseParallax from 'vue-mouse-parallax'
+import Vuex from 'vuex'
 import style from './style.css'
 import normalize from './normalize.css'
 
-
-
 Vue.use(VueRouter)
 Vue.use(VueMouseParallax)
+Vue.use(Vuex);
+
+//components
+import Men from './components/Menu.vue'
 
 //Views
 import Main from './views/main/main.vue'
@@ -19,8 +22,32 @@ var router = new VueRouter({
   ]
 })
 
+const store = new Vuex.Store({
+  state: {
+    count: 2
+  },
+  getters:{
+    count(state) {
+      return state.count;
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   router: router,
+  components:{
+    Men
+  },
+  data(){
+    return{
+      menuOpen: false
+    }
+  }
   
 })
